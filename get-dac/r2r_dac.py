529d54e5f4cc271b96ec4bf1c81e9148c65a5916
trepnacrain <trepnacrain@gmail.com>
2026-05-11 07:08:09 +0300

    fixed

--- get-dac/r2r_dac.py
+++ get-dac/r2r_dac.py
@@ -1,14 +1,19 @@
 import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+#dac_bits = [16, 20, 21, 25, 26, 17, 27, 22]
+dynamic_range = 3.3
+
 
 class R2R_DAC:
     def __init__(self, gpio_bits, dynamic_range, verbose = False):
         self.gpio_bits = gpio_bits
         self.dynamic_range = dynamic_range
         self.verbose = verbose
-
+        
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
-    
+
     def set_number(self, number):
         v = [int(element) for element in bin(number)[2:].zfill(8)]
         GPIO.output(self.gpio_bits, v)
@@ -16,36 +21,34 @@ class R2R_DAC:
     
     def set_voltage(self, voltage):
         if not (0.0 <= voltage <= self.dynamic_range):
-            print(f'Напряжение выходит за динамический диапазон ЦАП (0.0 - {self.dynamic_range:.2f} В)')
-            print('устанавливаем 0.0 В')
-            GPIO.output(self.gpio_bits, 0)
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
+            print("Устанавлниваем 0.0 В")
             return 0
-
+        
         else:
-            n = int(voltage / self.dynamic_range* 255)
+            n = int(voltage / self.dynamic_range * 255)
             self.set_number(n)
         return n
-    
 
     def deinit(self):
         GPIO.output(self.gpio_bits, 0)
         GPIO.cleanup()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
-
+        
         while True:
             try:
-                voltage = int(input('Введите напряжение в Вольтах:'))
+                voltage = float(input("Введите напряжение в Вольтах: "))
                 dac.set_voltage(voltage)
 
             except ValueError:
-                print('Вы ввели не число. Попробуйте еще раз\n')
+                print("Вы ввели не число. Попробуйте ещё раз\n")
 
     finally:
         dac.deinit()
 
 
-
+    
